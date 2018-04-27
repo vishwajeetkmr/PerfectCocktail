d7c7f1bc357529d914f4e64af49efab3b2586b9b
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2018-04-28 00:45:49 +0530

    Added Event Listeners

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Instantiate the classes
+const ui = new UI();
+
+
+// Create the Event Listeners
+function eventListeners() {
+
+// Add eventListeners when form is submitted
+const searchForm = document.querySelector('#search-form');
+if(searchForm){
+searchForm.addEventListener('submit', getCocktails);
+  }
+}
+
+eventListeners();
+
+
+
+// Get Cocktails
+function getCocktails(e){
+  e.preventDefault();
+
+  const searchTerm = document.querySelector('#search').value;
+
+  // Check something is in search form
+
+  if(searchTerm === ''){
+    ui.printMessage();
+  }else {
+    console.log('Query the REST API');
+  }
+
+}
